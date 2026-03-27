426befc312a8b859fb765450329a3e6d38bccb25
Paul Cheney <spartandesign.biz@gmail.com>
2026-03-27 11:41:35 -0600

    dark mode

--- scripts/darkmode.js
+++ scripts/darkmode.js
@@ -1,9 +1,20 @@
-// Store the selected elements that we are going to use. 
+// Grab a reference to the selected elements that we are going to use. 
 const darkbtn = document.querySelector('#dark');
 const darkel = document.querySelector('body')
 
+// Load saved preference from local storage
+if (localStorage.getItem('theme') === 'dark') {
+	darkel.classList.add('dark');
+}
 
-//Toggle the show class off and on
 darkbtn.addEventListener('click', () => {
 	darkel.classList.toggle('dark');
+
+  if (darkel.classList.contains('dark')) {
+    localStorage.setItem('theme', 'dark');
+  } else {
+    localStorage.setItem('theme', 'light');
+  }
 });
+
+
