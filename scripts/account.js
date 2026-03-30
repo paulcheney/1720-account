57243a646aac973d3e1029718e56f5332c9dd10b
Paul Cheney <spartandesign.biz@gmail.com>
2026-03-30 14:22:38 -0600

    xfhm

--- scripts/account.js
+++ scripts/account.js
@@ -1,51 +1,54 @@
-const signup = document.querySelector('#signup')
-const submitBtn = document.querySelector('#signup input[type=submit]')
+// GRAB REFERENCES TO THE SIGN UP FORM
+const signup = document.querySelector("#signup");
+const submitBtn = document.querySelector("#signup input[type=submit]");
+submitBtn.addEventListener("click", createProfile);
 
-const display = document.querySelector('#display')
-const deleteBtn = document.querySelector('#display > button')
+//GRAB REFERENCES TO THE DELETE LOCAL STORAGE
+const display = document.querySelector("#display");
+const deleteBtn = document.querySelector("#display > button");
+deleteBtn.addEventListener("click", deleteProfile);
 
 
-const myaccount = localStorage.getItem("yl-Name");
 
+//CHECK TO SEE IF LOCAL STORATE DATA ALREADY EXISTS
+const myaccount = localStorage.getItem("ly-Name");
+
+// DO THE SWITCHEROO
 if (myaccount === null) {
-    signup.className = "show"
-    display.className = "hide"
-    submitBtn.addEventListener("click", () => createProfile())
-
+  signup.className = "show";
+  display.className = "hide";
 } else {
-    signup.className = "hide"
-    display.className = "show"
-    const name = document.querySelector('#myName')
-    name.innerHTML = localStorage.getItem('yl-Name')
+  signup.className = "hide";
+  display.className = "show";
+  //Grab Values from local storage
+  const name = document.querySelector("#myName");
+  name.innerHTML = localStorage.getItem("ly-Name");
 
-    const email = document.querySelector('#myEmail')
-    email.innerHTML = localStorage.getItem('yl-Email')
+  const email = document.querySelector("#myEmail");
+  email.innerHTML = localStorage.getItem("ly-Email");
 
-    const phone = document.querySelector('#myPhone')
-    phone.innerHTML = localStorage.getItem('yl-Phone')
-
-    deleteBtn.addEventListener("click", () => {
-        localStorage.removeItem("yl-Name")
-        localStorage.removeItem("yl-Email")
-        localStorage.removeItem("yl-Phone")
-        window.location.reload()
-    })
+  const phone = document.querySelector("#myPhone");
+  phone.innerHTML = localStorage.getItem("ly-Phone");
 }
 
+// FUNCTION TO CREATE A NEW PROFILE
 function createProfile() {
-    const name = document.querySelector('#name')
-    const email = document.querySelector('#email')
-    const phone = document.querySelector('#phone')
+  const name = document.querySelector("#name");
+  const email = document.querySelector("#email");
+  const phone = document.querySelector("#phone");
 
-    if (!name.value || !email.value || !phone.value) {
-        console.log('Fill out the form')
-        
-    } else {
-        localStorage.setItem('yl-Name', name.value)
-        localStorage.setItem('yl-Email', email.value)
-        localStorage.setItem('yl-Phone', phone.value)
-        window.location.reload()
-    }
+  if (name.value && email.value && phone.value) {
+    localStorage.setItem("ly-Name", name.value);
+    localStorage.setItem("ly-Email", email.value);
+    localStorage.setItem("ly-Phone", phone.value);
+    window.location.reload();
+  }
+}
 
-
+// FUNCTION TO DELETE A PROFILE
+function deleteProfile() {
+  localStorage.removeItem("ly-Name");
+  localStorage.removeItem("ly-Email");
+  localStorage.removeItem("ly-Phone");
+  window.location.reload();
 }
